d73a3994bd04ab68e6abc9f6e26c0e82d4c9afe6
maximsmirin <42245917+maximsmirin@users.noreply.github.com>
2018-09-03 19:00:31 +0300

    Update GildedRoseTest.java

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    /*@Test*/
+    @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
 }
